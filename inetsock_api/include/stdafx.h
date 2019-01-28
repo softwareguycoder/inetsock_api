c5916e763f5e543ba12b64c2f2caab158f0b76a7
softwareguycoder <softwareguycoder@gmail.com>
2019-01-28 15:15:11 -0500

    28 Jan 2019 Initial create of inetsock_api lib

---
+++ inetsock_api/include/stdafx.h
@@ -0,0 +1,21 @@
+#ifndef __INETSOCK_API_STDAFX_H__
+#define __INETSOCK_API_STDAFX_H__
+
+#define _GNU_SOURCE
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+//#include <../../debug_core/debug_core/include/debug_core.h>
+
+#endif //__INETSOCK_API_STDAFX_H__
